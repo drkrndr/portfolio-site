7d86bd65e2fc82d17ca3d4ad30d4b855698da3b0
Nyjai Walls <144952455+conwalls@users.noreply.github.com>
2025-09-14 22:54:09 -0400

    Deploy fully built Svelte portfolio

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import adapter from "@sveltejs/adapter-static";
 export default {
   kit: {
     adapter: adapter({
-      // default options are fine
-      pages: "build", // folder where static site will be output
+      pages: "build",
       assets: "build",
       fallback: null,
     }),
