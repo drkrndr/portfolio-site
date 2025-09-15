3ccf0368feb49c05d201ea49a28defc4e0796647
Nyjai Walls <144952455+conwalls@users.noreply.github.com>
2025-09-14 23:29:14 -0400

    Deploy: 2025-09-14 23:29:14

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
-  base: "./",
+  base: "/portfolio-site/",
+  publicDir: "public",
   build: {
     outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true
   },
+  server: {
+    port: 3000
+  }
 });
